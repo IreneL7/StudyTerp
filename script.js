084013d6b395b765752f89f8ba0c93a338b1388d
IreneL7 <irene.haebin.lee@gmail.com>
2023-10-22 05:57:07 -0400

    updated terpmatchmaker section

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+
+
+// action event listener when user clicks submit button, run addStudentToDatabase function
+
+
+function addStudentToDatabase() {
+    let fName = document.getElementById("firstName");
+    let lName = document.getElementById("lastName");
+    let uid = document.getElementById("UID");
+    output.innerHTML = "<tr><td>"+fName+"<tr><td>"+lName+"<tr><td>"+uid;
+}
